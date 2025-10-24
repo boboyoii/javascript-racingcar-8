deac7bd134cc54f76829a386d0896c2c925cba74
boyeon <boyeon6331@gamil.com>
2025-10-24 14:29:32 +0900

    feat: 게임 서비스 기본 속성 초기화 기능 추가 - 자동차 객체 등록 기능 - 라운드 수 설정 기능

---
+++ src/service/GameService.js
@@ -0,0 +1,19 @@
+import Car from "../model/Car.js";
+
+class GameService {
+  constructor() {
+    this.cars = [];
+    this.totalRounds = 0;
+    this.currentRound = 0;
+  }
+
+  setCars(names) {
+    this.cars = names.map((name) => new Car(name));
+  }
+
+  setRoundCount(count) {
+    this.totalRound = count;
+  }
+}
+
+export default GameService;
